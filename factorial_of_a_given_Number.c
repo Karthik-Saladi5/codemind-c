1d7eaf3921df4a90aaa1495610d656518a64c1fe
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2022-12-22 10:03:23 -0800

    #include<stdio.h> int main() {     int i,n,res;     scanf("%d",&n);     i=n;     res=1;     while(i>=1)     {         res=res*i;         i--;     }     printf("%d",res); }

---
+++ factorial_of_a_given_Number.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+int main()
+{
+    int i,n,res;
+    scanf("%d",&n);
+    i=n;
+    res=1;
+    while(i>=1)
+    {
+        res=res*i;
+        i--; 
+    }
+    printf("%d",res);
+}

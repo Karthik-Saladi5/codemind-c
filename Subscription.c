ffa80e9d897fecc428d870de7bb7f8a2fec43763
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-19 15:33:12 +0530

    #include<stdio.h> int main()     {         int t,i,x;         scanf("%d",&t);         for(i=1;i<=t;i++)         {             scanf("%d",&x);             if(x>30) printf("YES");             else printf("NO");             printf(" ");         }     }

---
+++ Subscription.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+    {
+        int t,i,x;
+        scanf("%d",&t);
+        for(i=1;i<=t;i++)
+        {
+            scanf("%d",&x);
+            if(x>30) printf("YES");
+            else printf("NO");
+            printf("
+");
+        }
+    }

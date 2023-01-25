497b6efdeb3e2b4e1becb646c216fdc60d98edca
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-25 23:51:48 +0530

    #include<stdio.h> void ap(int r) {     float a,p;     p=2*3.14*r;     a=3.14*r*r;     printf("%0.2f %0.2f",a,p); } int main() {     int r;     scanf("%d",&r);     ap(r); }

--- Area_and_Perimeter_of_Circle.c
+++ Area_and_Perimeter_of_Circle.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-int main()
+void ap(int r)
 {
-    float r,p,a;
-    scanf("%f",&r);
+    float a,p;
     p=2*3.14*r;
     a=3.14*r*r;
     printf("%0.2f
 %0.2f",a,p);
+}
+int main()
+{
+    int r;
+    scanf("%d",&r);
+    ap(r);
 }

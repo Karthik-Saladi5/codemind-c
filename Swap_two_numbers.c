74aadd5c5d4d2d1067e3e55be8727d855504539d
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-28 23:58:21 +0530

    #include<stdio.h> void change(int a,int b) {     a=a+b;     b=a-b;     a=a-b;     printf("%d %d",a,b); } int main() {     int a,b;     scanf("%d%d",&a,&b);     change(a,b); }

--- Swap_two_numbers.c
+++ Swap_two_numbers.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+void change(int a,int b)
+{
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("%d
+%d",a,b);
+}
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    printf("%d
-%d",b,a);
+    change(a,b);
 }

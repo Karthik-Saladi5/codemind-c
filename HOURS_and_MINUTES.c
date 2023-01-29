af60ca3a6ba89bc1b7196c2ff0917c5e8a334279
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-29 22:51:39 +0530

    #include<stdio.h> void tym(int s) {     int h,m;     h=s/60;     m=s%60;     printf("%d Hour(s) %d Minute(s)",h,m); } int main() {     int s;     scanf("%d",&s);     tym(s); }

--- HOURS_and_MINUTES.c
+++ HOURS_and_MINUTES.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
-int main()
+void tym(int s)
 {
-    int s,h,m;
-    scanf("%d",&s);
+    int h,m;
     h=s/60;
     m=s%60;
     printf("%d Hour(s) %d Minute(s)",h,m);
+}
+int main()
+{
+    int s;
+    scanf("%d",&s);
+    tym(s);
 }

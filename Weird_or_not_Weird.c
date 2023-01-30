3abf03faf4df746c1357d49ce17b6da4ad2d0992
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-30 22:20:03 +0530

    #include<stdio.h> int wnw( int a) {     int b;     b=a%2;     if(b!=0) return 1;     else if(b==0 && a>=2 && a<=5) return 0;     else if(b==0 && a>=6 && a<=20) return 1;     else if(b==0 && a>20) return 0; } int main() {     int a;     scanf("%d",&a);     int x=wnw(a);     if (x==0) printf("not weird");     else printf("weird"); }

--- Weird_or_not_Weird.c
+++ Weird_or_not_Weird.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
+int wnw( int a)
+{
+    int b;
+    b=a%2;
+    if(b!=0) return 1;
+    else if(b==0 && a>=2 && a<=5) return 0;
+    else if(b==0 && a>=6 && a<=20) return 1;
+    else if(b==0 && a>20) return 0;
+}
 int main()
 {
-    int a,b;
+    int a;
     scanf("%d",&a);
-    b=a%2;
-    if(b!=0)
-    {
-        printf("weird");
-    }
-    else if(b==0 && a>=2 && a<=5)
-    {
-        printf("not weird");
-    }
-    else if(b==0 && a>=6 && a<=20)
-    {
-        printf("weird");
-    }
-    else if(b==0 && a>20)
-    {
-        printf("not weird");
-    }
+    int x=wnw(a);
+    if (x==0) printf("not weird");
+    else printf("weird");
 }

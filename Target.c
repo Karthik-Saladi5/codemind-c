f2f763124bc93396f9dd222328254075f74b8c08
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-30 22:15:40 +0530

    #include<stdio.h> int target(int p, int q, int r, int s) { if (p>=10 && q>=10 && r>=10 && s>=10)     {         return 1;     }     else     {         return 0;     } } int main() {     int p,q,r,s;     scanf("%d%d%d%d",&p,&q,&r,&s);     int x=target(p,q,r,s);     if(x==1)     printf("YES");     else     printf("NO"); }

--- Target.c
+++ Target.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
+int target(int p, int q, int r, int s)
+{
+if (p>=10 && q>=10 && r>=10 && s>=10)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
 int main()
 {
     int p,q,r,s;
     scanf("%d%d%d%d",&p,&q,&r,&s);
-    if (p>=10 && q>=10 && r>=10 && s>=10)
-    {
-        printf("YES");
-    }
-    else 
-    {
-        printf("NO");
-    }
+    int x=target(p,q,r,s);
+    if(x==1)
+    printf("YES");
+    else
+    printf("NO");
 }

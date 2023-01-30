2522cf355e6f3375e05e11ccc935a373c2b90809
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-30 22:18:04 +0530

    #include<stdio.h> int cas(char a) {      if (a>='A' && a<='Z') return 1;     else if(a>='a' && a<='z') return 2;     else return 0; } int main() {     char a;     scanf("%c",&a);     int x=cas(a);     if(x==1) printf("uppercase alphabet");     else if(x==2) printf("lowercase alphabet");     else printf("not an alphabet"); }

--- Character_is_Uppercase_or_Lowercase_.c
+++ Character_is_Uppercase_or_Lowercase_.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
+int cas(char a)
+{
+     if (a>='A' && a<='Z') return 1;
+    else if(a>='a' && a<='z') return 2;
+    else return 0;
+}
 int main()
 {
     char a;
     scanf("%c",&a);
-    if (a>='A' && a<='Z')
-    {
-        printf("uppercase alphabet");
-    }
-    else if(a>='a' && a<='z')
-    {
-        printf("lowercase alphabet");
-    }
-    else 
-    {
-        printf("not an alphabet");
-    }
+    int x=cas(a);
+    if(x==1) printf("uppercase alphabet");
+    else if(x==2) printf("lowercase alphabet");
+    else printf("not an alphabet");
 }

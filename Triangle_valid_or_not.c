e9502e4f0544382586d69fb2629233f0146a0587
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-31 23:58:05 +0530

    #include<stdio.h> int valid(float a,float b,float c) {     if ((a+b)>c && (a+c)>b && (b+c)>a)     return 1;     else     return 0; } int main() {     float a,b,c;     scanf("%f%f%f",&a,&b,&c);     int x=valid(a,b,c);     if (x==1)     printf("Valid triangle");     else     printf("Invalid triangle"); }

--- Triangle_valid_or_not.c
+++ Triangle_valid_or_not.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
+int valid(float a,float b,float c)
+{
+    if ((a+b)>c && (a+c)>b && (b+c)>a)
+    return 1;
+    else
+    return 0;
+}
 int main()
 {
     float a,b,c;
     scanf("%f%f%f",&a,&b,&c);
-    if ((a+b)>c && (a+c)>b && (b+c)>a)
-    {
-        printf("Valid triangle");
-    }
+    int x=valid(a,b,c);
+    if (x==1)
+    printf("Valid triangle");
     else
-    {
-        printf("Invalid triangle");
-    }
+    printf("Invalid triangle");
 }

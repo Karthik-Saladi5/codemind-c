71717132c890a59ab8caffbcefb6b6a224eb61f8
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-02-01 00:01:38 +0530

    #include<stdio.h> int big(int a,int b,int c) {     if(a>b)     {         if(a>c)         return 1;         else         return 2;     }     else     {         if(b>c)         return 3;         else         return 4;     } } int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int x=big(a,b,c);     if(x==1)     printf("%d",a);     else if (x==2)     printf("%d",c);     else if(x==3)     printf("%d",b);     else if (x==4)     printf("%d",c);  }

--- Largest_number_among_3_numbers.c
+++ Largest_number_among_3_numbers.c
@@ -1,28 +1,32 @@
 #include<stdio.h>
+int big(int a,int b,int c)
+{
+    if(a>b)
+    {
+        if(a>c)
+        return 1;
+        else
+        return 2;
+    }
+    else
+    {
+        if(b>c)
+        return 3;
+        else
+        return 4;
+    }
+}
 int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if(a>b)
-    {
-        if(a>c)
-        {
-            printf("%d",a);
-        }
-        else//c>a
-        {
-            printf("%d",c);
-        }
-    }
-    else//a<b
-    {
-        if(b>c)
-        {
-            printf("%d",b);
-        }
-        else//b<c
-        {
-            printf("%d",c);
-        }
-    }
-}
+    int x=big(a,b,c);
+    if(x==1)
+    printf("%d",a);
+    else if (x==2)
+    printf("%d",c);
+    else if(x==3)
+    printf("%d",b);
+    else if (x==4)
+    printf("%d",c);
+ }

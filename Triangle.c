645e906a3e7ce3ed1409fb607271e17cc26c48df
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-31 23:59:11 +0530

    #include<stdio.h> int type(int a, int b, int c) {     if (a==b && b==c && a==c)     return 1;     else if (a==b || b==c || a==c)     return 2;     else if (a!=b!=c)     return 3; } int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int x=type(a,b,c);     scanf("%d%d%d",&a,&b,&c);     if (x==1)     printf("Equilateral triangle");     else if (x==2)     printf("Isosceles triangle");     else if (x==3)     printf("Scalene triangle"); }

--- Triangle.c
+++ Triangle.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
+int type(int a, int b, int c)
+{
+    if (a==b && b==c && a==c)
+    return 1;
+    else if (a==b || b==c || a==c)
+    return 2;
+    else if (a!=b!=c)
+    return 3;
+}
 int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if (a==b && b==c && a==c)
-    {
-        printf("Equilateral triangle");
-    }
-    else if (a==b || b==c || a==c)
-    {
-        printf("Isosceles triangle");
-    }
-    else if (a!=b!=c)
-    {
-        printf("Scalene triangle");
-    }
+    int x=type(a,b,c);
+    scanf("%d%d%d",&a,&b,&c);
+    if (x==1)
+    printf("Equilateral triangle");
+    else if (x==2)
+    printf("Isosceles triangle");
+    else if (x==3)
+    printf("Scalene triangle");
 }

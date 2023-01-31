0ba5f3db6432d7059df17573ec6e8bce770ae918
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-01-31 23:59:48 +0530

    #include<stdio.h> float bill(int u) {     float b,s,tb;     if(u<199)         b=u*1.2;     else if(u>=200 && u<400)
            b=u*1.5;
        
        else if(u>=400 && u<600)
        
            b=u*1.8;
        
        else
        
            b=u*2.0;
        
        if(b>400)
        
            s=b*0.15;
        
        else
        
            s=100;
        
        tb=b+s;
        return tb;
    }
    int main()
    {
        int u;
        scanf("%d",&u);
        float x=bill(u);
        printf("%.2f",x);
    }

--- Current_bill.c
+++ Current_bill.c
@@ -1,33 +1,36 @@
 #include<stdio.h>
+float bill(int u)
+{
+    float b,s,tb;
+    if(u<199)
+        b=u*1.2;
+    else if(u>=200 && u<400)
+    
+        b=u*1.5;
+    
+    else if(u>=400 && u<600)
+    
+        b=u*1.8;
+    
+    else
+    
+        b=u*2.0;
+    
+    if(b>400)
+    
+        s=b*0.15;
+    
+    else
+    
+        s=100;
+    
+    tb=b+s;
+    return tb;
+}
 int main()
 {
     int u;
-    float b, s, tb;
     scanf("%d",&u);
-    if(u<199)
-    {
-        b=u*1.2;
-    }
-    else if(u>=200 && u<400)
-    {
-        b=u*1.5;
-    }
-    else if(u>=400 && u<600)
-    {
-        b=u*1.8;
-    }
-    else
-    {
-        b=u*2.0;
-    }
-    if(b>400)
-    {
-        s=b*0.15;
-    }
-    else
-    {
-        s=100;
-    }
-    tb=b+s;
-    printf("%.2f",tb);
+    float x=bill(u);
+    printf("%.2f",x);
 }

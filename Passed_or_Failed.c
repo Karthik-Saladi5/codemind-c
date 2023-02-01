dfa58c72e238e5932e7343dc94d2d76f0ea9f4d0
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-02-01 22:58:26 +0530

    #include<stdio.h> int pof(int a, int b, int c, int d, int e) {     if (a>34 && b>34 && c>34 && d>34 && e>34)     return 1;     else     return 0; } int main() {     int a,b,c,d,e;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     int x=pof(a,b,c,d,e);     if (x==1)     printf("PASSED");     else if(x==0)     printf("FAILED"); }

--- Passed_or_Failed.c
+++ Passed_or_Failed.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
+int pof(int a, int b, int c, int d, int e)
+{
+    if (a>34 && b>34 && c>34 && d>34 && e>34)
+    return 1;
+    else
+    return 0;
+}
 int main()
 {
     int a,b,c,d,e;
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-    if (a>34 && b>34 && c>34 && d>34 && e>34)
-    {
-        printf("PASSED");
-    }
-    else
-    {
-        printf("FAILED");
-    }
+    int x=pof(a,b,c,d,e);
+    if (x==1)
+    printf("PASSED");
+    else if(x==0)
+    printf("FAILED");
 }

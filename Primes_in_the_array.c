fa29db941a93caff92fbfed42e31e53a38f9a615
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2023-06-16 12:12:41 +0530

    #include<stdio.h> #include<stdbool.h> bool isprime(int n){     int i,c=0;     for(i=2;i<n;i++)     {         if(n%i==0)         c++;     }     if(c==0)     return 1;     else     return 0; } int main(){     int n;     scanf("%d",&n);     int a[n],c=0,k;     for(int i=0;i<n;i++){         scanf("%d",&a[i]);         if(isprime(a[i]) && a[i]!=1) c++;     }     printf("%d",c); }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool isprime(int n){
+    int i,c=0;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        c++;
+    }
+    if(c==0)
+    return 1;
+    else
+    return 0;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n],c=0,k;
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(isprime(a[i]) && a[i]!=1) c++;
+    }
+    printf("%d",c);
+}
